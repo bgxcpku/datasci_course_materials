3a49bc0c4b8841710f75453e453cd1215810e3c1
Steve Xie <bgxcpku@gmail.com>
2015-06-08 23:37:03 -0400

    finish

---
+++ assignment3/invert.py
@@ -0,0 +1,37 @@
+import MapReduce
+import sys
+
+"""
+Create an Inverted index. Given a set of documents, 
+an inverted index is a dictionary where each word 
+is associated with a list of the document identifiers
+in which that word appears.
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record[0]
+    value = record[1]
+    words = value.split()
+    for w in words:
+      mr.emit_intermediate(w, key)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+    total = []
+    for v in list_of_values:
+      total.append(v)
+    mr.emit((key, total))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
